7f8290fe91c999a5a9a2a0d80d7f75531e94e30b
Marco <Marco@LAPTOP-FNODT5BF.station>
2021-05-10 22:22:57 +0200

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -15,6 +15,26 @@ public Libretto() {
     	this.voti.add(v);
     }
     
+    public List<Voto> listaVotiUguali(int punteggio) {
+    	List<Voto> risultato = new ArrayList<Voto>();
+    	for(Voto v : this.voti) {
+    		if(v.getVoto() == punteggio) {
+    			risultato.add(v);    		
+    			}
+    	}
+    	return risultato;
+    }
+    
+    public Libretto votiUguali(int punteggio) {
+    	Libretto risultato = new Libretto();
+    	for(Voto v : this.voti) {
+    		if(v.getVoto() == punteggio) {
+    			risultato.add(v);
+    		}
+    	}
+    	return risultato;
+    }
+    
     public String toString() {
     	String s = " ";
     	for(Voto v : this.voti) {
